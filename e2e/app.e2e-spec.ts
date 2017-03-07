ffbce5cdeb0e05dcf86d35dbebae103a2f73a079
Angular CLI <angular-cli@angular.io>
2017-03-06 17:04:19 -0800

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { MacheteUiPage } from './app.po';
+
+describe('machete-ui App', () => {
+  let page: MacheteUiPage;
+
+  beforeEach(() => {
+    page = new MacheteUiPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
