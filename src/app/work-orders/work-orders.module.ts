53c4c46c8868cd4b55651c7e3ae59a33ced573a8
jcii <jciispam@gmail.com>
2017-07-22 10:16:52 -0700

    minimal datatable setup for main work-orders module

--- src/app/work-orders/work-orders.module.ts
+++ src/app/work-orders/work-orders.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkOrdersComponent } from './work-orders.component';
 import { WorkOrdersRoutingModule } from "./work-orders-routing.module";
+import { DataTableModule } from 'primeng/primeng';
 
 @NgModule({
   imports: [
     CommonModule,
+    DataTableModule,
     WorkOrdersRoutingModule
   ],
   declarations: [WorkOrdersComponent]
