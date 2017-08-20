a7adc96edab422a45803178433433ad11f96d718
Jimmy Carter <jciispam@gmail.com>
2017-08-19 22:13:03 -0700

    adding configs boilerplate

---
+++ src/app/configs/configs.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConfigsComponent } from '../src/app/configs/configs.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ConfigsComponent]
+})
+export class ConfigsModule { }
