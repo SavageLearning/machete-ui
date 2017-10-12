fb700525865918275923fb270d7d9a8f6a44b9ae
jcii <jciispam@gmail.com>
2017-10-12 14:06:58 -0700

    tests pass

--- src/app/online-orders/work-assignments/work-assignments.component.spec.ts
+++ src/app/online-orders/work-assignments/work-assignments.component.spec.ts
@@ -12,7 +12,8 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { WorkOrderService } from '../work-order/work-order.service';
 import { OnlineOrdersService } from '../online-orders.service';
 import { loadTransportRules } from '../shared';
-import { WorkOrderServiceSpy, WorkAssignmentsServiceSpy, LookupsServiceSpy } from "../../shared/testing";
+import { WorkOrderServiceSpy, WorkAssignmentsServiceSpy, LookupsServiceSpy, OnlineOrdersServiceSpy, RouterSpy } from "../../shared/testing";
+import { Router } from "@angular/router";
 
 describe('WorkAssignmentsComponent', () => {
   let component: WorkAssignmentsComponent;
@@ -35,6 +36,8 @@ describe('WorkAssignmentsComponent', () => {
           { provide: WorkAssignmentsService, useClass: WorkAssignmentsServiceSpy },
           { provide: LookupsService, useClass: LookupsServiceSpy },
           { provide: WorkOrderService, useClass: WorkOrderServiceSpy },
+          { provide: OnlineOrdersService, useClass: OnlineOrdersServiceSpy },
+          { provide: Router, useClass: RouterSpy }
         ]
       }
     })
