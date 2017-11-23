da132069ea345676d06d3a1d8fcbfc720676efff
Jimmy Carter <jciispam@gmail.com>
2017-11-22 21:50:21 -0800

    added simplest http.get

--- src/app/online-orders/scheduling.service.ts
+++ src/app/online-orders/scheduling.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { ScheduleRule } from './shared/index';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class SchedulingService {
+  uriBase = environment.dataUrl + '/api/schedulerules';
+  constructor(private http: HttpClient) {
+  }
 
-  constructor() { }
-
+  getScheduleRules(): Observable<ScheduleRule[]> {
+    return this.http.get(this.uriBase)
+      .map(res => res['data'] as ScheduleRule[]);
+  }
 }
