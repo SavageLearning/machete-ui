037aea1679b8805e8bdfca6f6a30a4d5e5999681
Jimmy Carter <jciispam@gmail.com>
2017-11-22 22:03:37 -0800

    Adding code to cache rules in object (not storage)

--- src/app/online-orders/scheduling.service.ts
+++ src/app/online-orders/scheduling.service.ts
@@ -3,15 +3,38 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { ScheduleRule } from './shared/index';
 import { environment } from '../../environments/environment';
+import { HandleError } from '../shared/handle-error';
 
 @Injectable()
 export class SchedulingService {
   uriBase = environment.dataUrl + '/api/schedulerules';
+  rules = new Array<ScheduleRule>();
+  rulesAge = 0;
   constructor(private http: HttpClient) {
   }
 
+  isStale(): boolean {
+    if (this.rulesAge > Date.now() - 36000) {
+        return false;
+    }
+    return true;
+  }
+
+  isNotStale(): boolean {
+    return !this.isStale();
+  }
+
   getScheduleRules(): Observable<ScheduleRule[]> {
+    if (this.isNotStale()) {
+      return Observable.of(this.rules);
+    }
+
     return this.http.get(this.uriBase)
-      .map(res => res['data'] as ScheduleRule[]);
+      .map(res => {
+        this.rules = res['data'] as ScheduleRule[];
+        this.rulesAge = Date.now();
+        return res['data'] as ScheduleRule[];
+      })
+      .catch(HandleError.error);
   }
 }
