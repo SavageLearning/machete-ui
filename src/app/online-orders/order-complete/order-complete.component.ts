3ec9247f5124e2a50559bb9a6e3e1753a4218c77
Jimmy Carter <jciispam@gmail.com>
2017-12-21 20:49:19 -0800

    ninjected paypal checkout.js and paypal express checkout button

--- src/app/online-orders/order-complete/order-complete.component.ts
+++ src/app/online-orders/order-complete/order-complete.component.ts
@@ -4,7 +4,7 @@ import { WorkOrder } from '../../shared/models/work-order';
 import { LookupsService } from '../../lookups/lookups.service';
 import { LCategory } from '../../lookups/models/lookup';
 import { Observable } from 'rxjs/Observable';
-
+declare let paypal: any;
 @Component({
   selector: 'app-order-complete',
   templateUrl: './order-complete.component.html',
@@ -15,8 +15,38 @@ export class OrderCompleteComponent implements OnInit {
   order = new WorkOrder();
   transportLabel: string;
   workerCount: number;
-  transportCost: number;
+  transportCost: number = 0;
   laborCost: number;
+  // paypal values
+  public didPaypalScriptLoad: boolean = false;
+  public loading: boolean = true;
+
+  public paypalConfig: any = {
+    env: 'sandbox',
+    client: {
+      sandbox:    'AeabfiAbx3eY7bFZDsns0L4u77c4TE4cLuU8bZ4hWA1u9D5kVA2_KbBIJh3mIJcjJ96fGEckqoi9ynyr',
+      production: 'AcXQ3nPggEKWs48Q6_L8F9nwXppmuLNCRAfhzIsOHejWYvUr7Ob1Ciekdc0v4lRliCl0nIW6abuKQeuM'
+    },
+    commit: true,
+    payment: (data, actions) => {
+      return actions.payment.create({
+        payment: {
+          transactions: [
+            { amount: { total: this.transportCost, currency: 'USD' } }
+          ]
+        }
+      });
+    },
+    onAuthorize: (data, actions) => {
+      console.log('Payment was successful!', data, actions);
+    },
+    onCancel: function(data) {
+      console.log('The payment was cancelled!', data);
+    },
+    onError: function(err) {
+      console.log('There was an error:', err);
+    }
+  };
 
   constructor(
     private onlineService: OnlineOrdersService,
@@ -50,6 +80,25 @@ export class OrderCompleteComponent implements OnInit {
       }
     },
     error => console.error('error', error));
+  }
+
+  public ngAfterViewChecked(): void {
+    if(!this.didPaypalScriptLoad) {
+      this.loadPaypalScript().then(() => {
+        paypal.Button.render(this.paypalConfig, '#paypal-button');
+        this.loading = false;
+      });
+    }
+  }
+
+  public loadPaypalScript(): Promise<any> {
+    this.didPaypalScriptLoad = true;
+    return new Promise((resolve, reject) => {
+      const scriptElement = document.createElement('script');
+      scriptElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scriptElement.onload = resolve;
+      document.body.appendChild(scriptElement);
+    });
   }
 
 }
