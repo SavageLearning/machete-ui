e0fc64f4359ace95a38f541b7b8c00f241fa647b
jcii <jciispam@gmail.com>
2017-12-30 10:34:38 -0800

    Added time parsing and sorting to /work-orders

--- src/app/work-orders/work-orders.module.ts
+++ src/app/work-orders/work-orders.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { WorkOrdersComponent } from './work-orders.component';
 import { WorkOrdersRoutingModule } from './work-orders-routing.module';
 import { DataTableModule } from 'primeng/primeng';
+import { MomentModule } from 'angular2-moment';
 
 @NgModule({
   imports: [
     CommonModule,
     DataTableModule,
-    WorkOrdersRoutingModule
+    WorkOrdersRoutingModule,
+    MomentModule
   ],
   declarations: [WorkOrdersComponent]
 })
