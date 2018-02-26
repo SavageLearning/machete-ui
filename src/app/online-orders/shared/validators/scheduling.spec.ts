aa14f9df525de9d94d170d6962ccc4462312026f
jcii <jciispam@gmail.com>
2018-02-25 22:53:15 -0800

    tests working, tweaking validator

--- src/app/online-orders/shared/validators/scheduling.spec.ts
+++ src/app/online-orders/shared/validators/scheduling.spec.ts
@@ -4,10 +4,40 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ScheduleRule } from '..';
 
 describe('ScheduleRule', () => {
+    let ctrl: FormControl;
+    let tFunc: ValidatorFn;
+    let today: Date;
+    beforeEach(() => {
+        ctrl = new FormControl();
+        today = new Date();
+        tFunc = schedulingValidator(new Array<ScheduleRule>(new ScheduleRule({
+            day: today.getUTCDay(),
+            leadHours: 48,
+            minStartMin: 420, //7am
+            maxEndMin: 1020 // 5pm 
+        })));    
+    });
+
   it('should create an instance', () => {
-    const ctrl = new FormControl();
-    const tFunc = schedulingValidator(new Array<ScheduleRule>());
     const result = tFunc(ctrl);
     expect(result).toBeNull();
+  });
+
+  it('should reject time in the past', () => {
+    let anHourAgo = today.valueOf() - (3600 * 1000);
+    ctrl.setValue((new Date(anHourAgo)).toLocaleString());
+    // act
+    const result = tFunc(ctrl);
+    // 
+    expect(result['scheduling']).toBe('Date cannot be in the past.');
+  });
+
+  it('should reject time before start time', () => {
+    var utc = today.toJSON().slice(0,10).replace(/-/g,'/');
+    ctrl.setValue(new Date(utc));
+    // act
+    const result = tFunc(ctrl);
+    // 
+    expect(result['scheduling']).toBe('Lead time of 0 hours less than the 2 days required.');
   });
 });
--- src/app/online-orders/shared/validators/scheduling.ts
+++ src/app/online-orders/shared/validators/scheduling.ts
@@ -1,24 +1,27 @@
 import { Directive, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { AbstractControl, NG_VALIDATORS, Validator, ValidatorFn, Validators } from '@angular/forms';
 import { ScheduleRule } from '../models/schedule-rule';
+import { request } from 'http';
 
 export function schedulingValidator(rules: ScheduleRule[]): ValidatorFn {
   return (control: AbstractControl): {[key: string]: any} => {
     if (control.value == null) {
         return null;
     }
-    const requestTime = control.value as Date;
-    const orderTime = Date.now();
+    const requestTime = new Date(control.value);
+    const orderTime = new Date();
     const leadInSecs = requestTime.valueOf() - orderTime.valueOf();
-
     const rule = rules.find(s => s.day === requestTime.getDay());
 
     if (leadInSecs < 0) {
         return {'scheduling': 'Date cannot be in the past.'}
     }
 
-    if (leadInSecs < (rule.leadHours * 3600)) {
-        return {'scheduling': 'Lead time less than ' + String(rule.leadHours) + ' hours.'}
+    let daysSinceEpoch = Math.floor(orderTime.valueOf()/8.64e7);
+    let reqDaysSinceEpoch = Math.floor(requestTime.valueOf()/8.64e7);
+    let leadInHours = (reqDaysSinceEpoch - daysSinceEpoch) * 24;
+    if (leadInHours < rule.leadHours) {
+        return {'scheduling': `Lead time of ${leadInHours} hours less than the ${rule.leadHours/24} days required.`}
     }
 
     if (requestTime.getHours() < (rule.minStartMin / 60)) {
