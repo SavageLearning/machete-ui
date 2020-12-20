a6f56d01b708de09293940b56fb8135a10676424
esteban-gs <comsat61@gmail.com>
2020-12-20 01:38:35 -0800

    cleaned the order confirm component UI

--- src/app/shared/components/work-orders/full-order-view/full-order-view.component.ts
+++ src/app/shared/components/work-orders/full-order-view/full-order-view.component.ts
@@ -2,6 +2,29 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { WorkOrder } from '../../../models/work-order';
 import {MessageService} from 'primeng/api';
 
+interface EmployerInfo {
+  name: string,
+  worksite1: string,
+  worksite2: string,
+  city: string,
+  state: string,
+  zip: string,
+  phone: string
+}
+
+interface JobDetails {
+  description: string,
+  englishRequired: string,
+  tranportMethod: string,
+  workerCount: number
+}
+
+interface OrderCost {
+  feeName: string,
+  fee: number,
+  due: string
+}
+
 @Component({
   selector: 'full-order-view',
   templateUrl: './full-order-view.component.html',
@@ -15,6 +38,10 @@ export class FullOrderViewComponent implements OnChanges {
   @Input() laborCost: number;
   @Input() showPayPal: boolean;
 
+  employerInfo: EmployerInfo;
+  jobInfo: JobDetails[] = new Array<JobDetails>();
+  orderCost: OrderCost[] = new Array<OrderCost>();
+
   constructor(private messageService: MessageService) {  
     console.log('.ctor');
    }
@@ -26,7 +53,35 @@ export class FullOrderViewComponent implements OnChanges {
       } else {
 
       }
+      this.employerInfo = {
+        name: this.order.contactName,
+        worksite1: this.order.worksiteAddress1,
+        worksite2: this.order.worksiteAddress2,
+        city: this.order.city,
+        state: this.order.state,
+        zip: this.order.zipcode,
+        phone: this.order.phone
+      };
+      this.jobInfo = [{
+        description: this.order.description,
+        englishRequired: this.order.englishRequired ? 'yes' : 'no',
+        tranportMethod: this.transportLabel,
+        workerCount: this.workerCount
+      }];
+      this.orderCost = [{
+        feeName: 'Transport Cost',
+        fee: this.transportCost,
+        due: 'Due now and payable with Paypal, debit or credit'
+      }, {
+        feeName: 'Labor Cost',
+        fee: this.laborCost,
+        due: 'To be paid directly to workers in cash at the end of the work day'
+      }];
+    }
+
+    print(printArea: string) {
+      let popupWinindow = document.getElementById(printArea).innerHTML;
+      window.print();
     }
 
 }
-
--- src/app/shared/components/work-orders/work-orders.module.ts
+++ src/app/shared/components/work-orders/work-orders.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import { FullOrderViewComponent } from './full-order-view/full-order-view.component';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+
+import { NgxPrintModule } from 'ngx-print';
 
 @NgModule({
   imports: [
     CommonModule,
     TableModule,
-    InputSwitchModule
+    InputSwitchModule,
+    ButtonModule,
+    NgxPrintModule
   ],
   declarations: [
     FullOrderViewComponent
