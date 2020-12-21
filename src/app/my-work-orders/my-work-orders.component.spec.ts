cfd98441f41e70f6bc7daac984e8e8f103968ecf
esteban-gs <comsat61@gmail.com>
2020-12-20 21:12:12 -0800

    MyWorkOrders cleaned up

--- src/app/my-work-orders/my-work-orders.component.spec.ts
+++ src/app/my-work-orders/my-work-orders.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MyWorkOrdersComponent } from './my-work-orders.component';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -9,7 +9,7 @@ describe('MyWorkOrdersComponent', () => {
   let component: MyWorkOrdersComponent;
   let fixture: ComponentFixture<MyWorkOrdersComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MyWorkOrdersComponent ],
       imports: [
