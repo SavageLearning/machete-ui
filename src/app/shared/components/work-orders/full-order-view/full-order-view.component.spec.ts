f0e2eba6fc82b8e5b6b5f47f436efe5b7879ad5a
esteban-gs <comsat61@gmail.com>
2020-12-28 22:48:29 -0800

    All tests passing

--- src/app/shared/components/work-orders/full-order-view/full-order-view.component.spec.ts
+++ src/app/shared/components/work-orders/full-order-view/full-order-view.component.spec.ts
@@ -5,14 +5,36 @@ import { FullOrderViewComponent } from './full-order-view.component';
 import { WorkOrder } from '../../../../shared/models/work-order';
 import { MessageService } from 'primeng/api';
 import { MessageServiceSpy } from '../../../testing';
+import { Component } from '@angular/core';
+
+// Arrange host wrapper component
+@Component({
+  template: `<full-order-view
+    [transportLabel]="transportLabel"
+    [workerCount]="workerCount"
+    [transportCost]="transportCost"
+    [laborCost]="laborCost"
+    [order]="order"
+    [showPayPal]="true">
+  </full-order-view>`,
+})
+
+export class TestWrapperComponent {
+  order: WorkOrder;
+  transportLabel: string;
+  workerCount: number;
+  transportCost: number;
+  laborCost: number;
+}
 
 describe('FullOrderViewComponent', () => {
-  let component: FullOrderViewComponent;
-  let fixture: ComponentFixture<FullOrderViewComponent>;
+  let component: TestWrapperComponent;
+  let fixture: ComponentFixture<TestWrapperComponent>;
+  let testWoDate: Date;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ FullOrderViewComponent ],
+      declarations: [ FullOrderViewComponent, TestWrapperComponent ],
       imports: [TableModule],
     }).overrideComponent(FullOrderViewComponent, {
       set: {
@@ -25,13 +47,29 @@ describe('FullOrderViewComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FullOrderViewComponent);
+    fixture = TestBed.createComponent(TestWrapperComponent);
     component = fixture.componentInstance;
     component.order = new WorkOrder();
+    component.order.contactName = 'testEmp';
+    testWoDate = new Date();
+    component.order.dateTimeofWork = testWoDate;
+    component.transportLabel = '';
+    component.workerCount = 2;
+    component.transportCost = 15;
+    component.laborCost = 150;
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display members', () => {
+    expect(component.order.contactName).toEqual('testEmp');
+    expect(component.order.dateTimeofWork).toEqual(testWoDate);
+    expect(component.transportLabel).toEqual('');
+    expect(component.workerCount).toEqual(2);
+    expect(component.transportCost).toEqual(15);
+    expect(component.laborCost).toEqual(150);
   });
 });
