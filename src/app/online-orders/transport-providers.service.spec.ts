1ed9b52102c2e2ce2925d4194b9e96fb2f5bea62
esteban-gs <comsat61@gmail.com>
2020-12-28 22:00:06 -0800

    all online-orders tests passing

--- src/app/online-orders/transport-providers.service.spec.ts
+++ src/app/online-orders/transport-providers.service.spec.ts
@@ -1,26 +1,26 @@
-// import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
-// import { TransportProvidersService } from './transport-providers.service';
-// import { HttpClientModule } from '@angular/common/http';
-// import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-// import { environment } from '../../environments/environment';
+import { TransportProvidersService } from './transport-providers.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from '../../environments/environment';
 
-// describe('TransportProviderService', () => {
-//   let httpMock: HttpTestingController;
-//   let baseref: string  = environment.dataUrl;
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [TransportProvidersService],
-//       imports: [
-//         HttpClientModule,
-//         HttpClientTestingModule
-//       ]
-//     })
-//     .compileComponents();
-//     httpMock = TestBed.inject(HttpTestingController);
-//   });
+describe('TransportProviderService', () => {
+  let httpMock: HttpTestingController;
+  let baseref: string  = environment.dataUrl;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TransportProvidersService],
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule
+      ]
+    })
+    .compileComponents();
+    httpMock = TestBed.inject(HttpTestingController);
+  });
 
-//   it('should be created', inject([TransportProvidersService], (service: TransportProvidersService) => {
-//     expect(service).toBeTruthy();
-//   }));
-// });
+  it('should be created', inject([TransportProvidersService], (service: TransportProvidersService) => {
+    expect(service).toBeTruthy();
+  }));
+});
