d8a2ffcb9ece731c20a79cdd59df5158122e96d8
esteban-gs <comsat61@gmail.com>
2020-12-31 02:14:51 -0800

    skills list with client side filters and sorting

--- src/app/workers/workers.component.ts
+++ src/app/workers/workers.component.ts
@@ -8,79 +8,23 @@ import { MegaMenuItem } from 'primeng/api';  //required when using MegaMenu
   styleUrls: ['./workers.component.css']
 })
 export class WorkersComponent implements OnInit {
-  items: MegaMenuItem[];
+  items: MenuItem[];
 
   constructor() { }
 
   ngOnInit() {
-    this.items = [
-      {
-        label: 'Skills',
-        icon: 'pi pi-list',
-        routerLink: ['skills'],
-        routerLinkActiveOptions: { exact: true }
-        // items: [
-        //   [
-        //     {
-        //       label: 'Video 1',
-        //       items: [{ label: 'Video 1.1' }, { label: 'Video 1.2' }]
-        //     },
-        //     {
-        //       label: 'Video 2',
-        //       items: [{ label: 'Video 2.1' }, { label: 'Video 2.2' }]
-        //     }
-        //   ],
-        //   [
-        //     {
-        //       label: 'Video 3',
-        //       items: [{ label: 'Video 3.1' }, { label: 'Video 3.2' }]
-        //     },
-        //     {
-        //       label: 'Video 4',
-        //       items: [{ label: 'Video 4.1' }, { label: 'Video 4.2' }]
-        //     }
-        //   ]
-        // ]
-      },
-      // {
-      //   label: 'Workers In Skill',
-      //   icon: 'pi pi-fw pi-users',
-      //   routerLink: ['in-skill/1'],
-      //   routerLinkActiveOptions: { exact: true }
-        // items: [
-        //   [
-        //     {
-        //       label: 'User 1',
-        //       items: [{ label: 'User 1.1' }, { label: 'User 1.2' }]
-        //     },
-        //     {
-        //       label: 'User 2',
-        //       items: [{ label: 'User 2.1' }, { label: 'User 2.2' }]
-        //     },
-        //   ],
-        //   [
-        //     {
-        //       label: 'User 3',
-        //       items: [{ label: 'User 3.1' }, { label: 'User 3.2' }]
-        //     },
-        //     {
-        //       label: 'User 4',
-        //       items: [{ label: 'User 4.1' }, { label: 'User 4.2' }]
-        //     }
-        //   ],
-        //   [
-        //     {
-        //       label: 'User 5',
-        //       items: [{ label: 'User 5.1' }, { label: 'User 5.2' }]
-        //     },
-        //     {
-        //       label: 'User 6',
-        //       items: [{ label: 'User 6.1' }, { label: 'User 6.2' }]
-        //     }
-        //   ]
-        // ]
-      // }
-    ]
+    this.items = [{
+        label: 'Workers',
+        icon: 'pi pi-fw pi-user',
+        items: [
+          {
+
+            label: 'Skills List',
+            icon: 'pi pi-list',
+            routerLink: ['skills'],
+            routerLinkActiveOptions: { exact: true }
+          }]
+    }]
   }
 
 }
--- src/app/workers/workers.module.ts
+++ src/app/workers/workers.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkersRoutingModule } from './workers-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { WorkersComponent } from './workers.component';
 import { SkillsListComponent } from './skills-list/skills-list.component';
 import { WorkersInSkillComponent } from './workers-in-skill/workers-in-skill.component';
-import { MegaMenuModule } from 'primeng/megamenu';
-import { MenuModule } from 'primeng/menu';
 
+import { MenuModule } from 'primeng/menu';
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { DialogModule } from 'primeng/dialog';
 
 
 @NgModule({
@@ -18,8 +26,17 @@ import { MenuModule } from 'primeng/menu';
   imports: [
     CommonModule,
     WorkersRoutingModule,
-    MegaMenuModule,
-    MenuModule
+    MenuModule,
+    CardModule,
+    TableModule,
+    ButtonModule,
+    InputTextModule,
+    ToolbarModule,
+    ToggleButtonModule,
+    FormsModule,
+    DialogModule
+  ],
+  providers: [
   ]
 })
-export class WorkersModule {}
+export class WorkersModule { }
