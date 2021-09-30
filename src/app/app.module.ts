54dc14f324bfa56ceca8b98e23cd0b534a07e52e
jcii <jciispam@gmail.com>
2021-09-29 23:29:04 -0700

    packaging cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppRoutingModule} from './app-routing.module';
-import { NgModule, Injector, ErrorHandler } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import {
+  AppMenuComponent,
+  AppSubMenuComponent,
+} from "./menu/app.menu.component";
+import { AppTopBarComponent } from "./app.topbar.component";
+import { AppFooterComponent } from "./app.footer.component";
+import { InlineProfileComponent } from "./menu/app.profile.component";
+import { PageNotFoundComponent } from "./not-found.component";
 
-// import { HttpClientModule, XHRBackend, BrowserXhr, ResponseOptions,  XSRFStrategy } from '@angular/http';
-import { AppComponent } from './app.component';
-import { AppMenuComponent, AppSubMenuComponent }  from './menu/app.menu.component';
-import { AppTopBarComponent }  from './app.topbar.component';
-import { AppFooterComponent }  from './app.footer.component';
-import { InlineProfileComponent }  from './menu/app.profile.component';
-import { PageNotFoundComponent }   from './not-found.component';
+import { ToastModule } from "primeng/toast";
 
-import { ToastModule } from 'primeng/toast';
-
-import { MessagesComponent } from './shared/components/messages/messages.component';
-import { MessagesService } from './shared/components/messages/messages.service';
+import { MessagesComponent } from "./shared/components/messages/messages.component";
+import { MessagesService } from "./shared/components/messages/messages.service";
+import { AuthorizeComponent } from "./auth";
+import { ReportsModule } from "./reports/reports.module";
+import { OnlineOrdersModule } from "./online-orders/online-orders.module";
+import { ExportsModule } from "./exports/exports.module";
+import { WorkersModule } from "./workers/workers.module";
+import { EmployersModule } from "./employers/employers.module";
+import { MyWorkOrdersModule } from "./my-work-orders/my-work-orders.module";
+import { AuthService, LoggingService } from "./shared";
+import { TransportProvidersService } from "./online-orders/transport-providers.service";
+import { MonacoEditorModule } from "ngx-monaco-editor";
+import { CardModule } from "primeng/card";
+import { DialogModule } from "primeng/dialog";
+import { MenubarModule } from "primeng/menubar";
 
 @NgModule({
   declarations: [
@@ -43,20 +57,22 @@ import { MessagesService } from './shared/components/messages/messages.service';
     WorkersModule,
     AppRoutingModule,
     ToastModule,
-    MonacoEditorModule.forRoot() // use forRoot() in main app module only.
+    CardModule,
+    DialogModule,
+    MenubarModule,
+    MonacoEditorModule.forRoot(), // use forRoot() in main app module only.
   ],
   providers: [
     AuthService,
     LoggingService,
     TransportProvidersService,
-    MessagesService
+    MessagesService,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {
   // Diagnostic only: inspect router configuration
   constructor() {
-    console.log('.ctor: AppModule');
+    console.log(".ctor: AppModule");
   }
-
 }
--- src/app/auth/authorize/authorize.component.ts
+++ src/app/auth/authorize/authorize.component.ts
@@ -1,37 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../shared/services/auth.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../shared/services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-authorize',
-  templateUrl: './authorize.component.html',
-  styleUrls: ['./authorize.component.css']
+  selector: "app-authorize",
+  templateUrl: "./authorize.component.html",
+  styleUrls: ["./authorize.component.css"],
 })
 export class AuthorizeComponent implements OnInit {
-
-  constructor(private auth: AuthService, private router: Router) { }
+  constructor(private auth: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     // copy to local; https://github.com/Microsoft/TypeScript/wiki/%27this%27-in-TypeScript
     const rtr = this.router;
 
-    this.auth.authorize().subscribe(user => {
-      user.state = user.state ? user.state : '/welcome';
+    this.auth.authorize().subscribe(
+      (user) => {
+        user.state = user.state ? user.state : "/welcome";
 
-      // get out of the auth loop (no hanging 'authorize works!')
-      if (user.state.endsWith('authorize')) {
-          user.state = '/welcome';
+        // get out of the auth loop (no hanging 'authorize works!')
+        if (user.state.endsWith("authorize")) {
+          user.state = "/welcome";
         }
 
-      if (user.profile.roles.includes('Hirer') && user.state === '/welcome') {
-        rtr.navigate(['/online-orders/introduction']).catch(e => console.error(e));
-      } else {
-        rtr.navigate([user.state]).catch(e => console.error(e));
+        if (user.profile.roles.includes("Hirer") && user.state === "/welcome") {
+          rtr
+            .navigate(["/online-orders/introduction"])
+            .catch((e) => console.error(e));
+        } else {
+          rtr.navigate([user.state]);
+        }
+      },
+      (err) => {
+        console.error(
+          "redirecting to login; endSigninMainWindow returned: ",
+          err
+        );
+        this.auth.removeUser();
+        rtr.navigate(["/welcome"]).catch((e) => console.error(e));
       }
-    }, err => {
-      console.error('redirecting to login; endSigninMainWindow returned: ', err);
-      this.auth.removeUser();
-      rtr.navigate(['/welcome']).catch(e => console.error(e));
-    });
+    );
   }
 }
